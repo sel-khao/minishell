e295581b079eb41afa341a6ee14f7229073dd176
sel-khao <sara.elkhy@gmail.com>
2025-05-03 13:13:42 +0200

    Add files via upload

--- lexer.c
+++ lexer.c
@@ -6,42 +6,52 @@
 /*   By: sel-khao <sel-khao <marvin@42.fr>>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 12:06:45 by sel-khao          #+#    #+#             */
-/*   Updated: 2025/05/03 12:50:55 by sel-khao         ###   ########.fr       */
+/*   Updated: 2025/05/03 13:11:49 by sel-khao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-char	quoted(char *line)
+int	ft_strlen(char *str)
 {
-	int i;
-	char *arg;
-	//int len;
+	int i = 0;
+	
+	while (str[i])
+		i++;
+	return (i);
+}
 
+char	*quoted(char *line)
+{
+	int		i;
+	int		j;
+	char	*arg;
+	int		len;
+
+	j = 0;
 	i = 0;
-	//arg = malloc(sizeof(char) * len);
+	len = ft_strlen(line);
+	arg = malloc(sizeof(char) * len + 1);
 	while (line[i])
 	{
-		if (line[i] == "'")
+		if (line[i] == '\'')
 		{
 			i++;
-			while (line[i] != "'")
-			{
-				arg = line[i];
-				i++;	
-			}
+			while (line[i] && line[i] != '\'')
+				arg[j++] = line[i++];
 		}
 		i++;
 	}
+	arg[j] = '\0';
 	return (arg);
 }
 
 int	main(int argc, char **argv)
 {
-	int i = 0;
-	
-	if (argc == 2)
-		quoted(argv);
-	return 0;
+	(void)argc;
+	char *result = quoted(argv[1]);
+	printf("%s\n", result);
+	free(result);
 }
